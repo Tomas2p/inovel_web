2a25deb89c03ecf9e552ebdf6a1bba01ffb7e40d
Donovan <donovanmar30@gmail.com>
2023-08-01 20:17:33 +0100

    Fix stories option choosing

--- scripts/story.js
+++ scripts/story.js
@@ -1,11 +1,9 @@
-/* const yaml = requiere('js-yaml');
-import fs from 'fs'; */
-
 class Story {
   constructor(data) {
     this.title = data.title;
     this.scenes = data.scenes;
     this.currentSceneIndex = 0;
+    this.currentChoicesIndex = 0;
   }
 
   run() {
@@ -16,41 +14,147 @@ class Story {
     const scene = this.scenes[this.currentSceneIndex];
     const container = document.getElementById("story-container");
 
-    // Limpiar el contenedor
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
+    if (!scene){
+
+      this.currentSceneIndex = 0;
+      this.currentChoicesIndex = 0;
+      clearOptions(container);
+      fetchStoryData();
+      
+    } else {
+
+      if (this.currentSceneIndex === 0){
+
+        clearOptions(container);
+
+        const titleElement = document.createElement("h1");
+        titleElement.textContent = this.title;
+        container.appendChild(titleElement);
+      } 
+
+      if (this.currentSceneIndex > 0) {
+        let sceneTitle = document.createElement("div");
+        sceneTitle.className = "scene-title";
+        let sceneParagraph = document.createElement("p");
+        let sceneTextNode = document.createTextNode(scene.title);
+        sceneTitle.appendChild(sceneTextNode);
+        container.children[1].appendChild(sceneTitle);
+      }
+
+      let sceneContainer;
+
+      if (!document.querySelector(".scene")) {
+      
+        const sceneElement = document.createElement("div");
+        sceneElement.className = "scene";
+        const sceneTextNode = document.createTextNode(scene.text);
+        sceneElement.appendChild(sceneTextNode);
+        container.appendChild(sceneElement);  
+      } else {
+        sceneContainer = document.querySelector(".scene");
+      }
+
+      if (scene.choices.length > 0) {
+        let optionsElement = printChoices(scene.choices, this);
+        container.appendChild(optionsElement);
+      }
+
     }
+  }
 
-    // Mostrar el título de la historia
-    const titleElement = document.createElement("h1");
-    titleElement.textContent = this.title;
-    container.appendChild(titleElement);
+  displayChoices(containerToPrintChoices, choices, story) {
 
-    // Mostrar el contenido de la escena
-    const sceneElement = document.createElement("div");
-    sceneElement.className = "scene";
-    sceneElement.innerHTML = scene.content;
-    container.appendChild(sceneElement);
+    this.currentChoicesIndex = 0;
+    clearOptions(containerToPrintChoices);
+    let newOptionsContainer = printChoices(choices, this);
 
-    // Mostrar las opciones de la escena
-    if (scene.options.length > 0) {
-      const optionsElement = document.createElement("div");
-      optionsElement.className = "options";
-      scene.options.forEach((option) => {
-        const optionButton = document.createElement("button");
-        optionButton.textContent = option.text;
-        optionButton.addEventListener("click", () => {
-          this.currentSceneIndex = option.nextScene;
-          this.displayScene();
-        });
-        optionsElement.appendChild(optionButton);
+    for (const childButton of newOptionsContainer.children) {
+
+      let nodeCloned = childButton.cloneNode(true);
+      nodeCloned.id = this.currentChoicesIndex;
+
+      nodeCloned.addEventListener("click", () => {
+      
+        this.currentChoicesIndex = nodeCloned.id;
+
+        if (choices[this.currentChoicesIndex].choices != null) {
+          
+          if (choices[this.currentChoicesIndex].choices.length > 0) {
+            
+            story.displayChoices(containerToPrintChoices, choices[this.currentChoicesIndex].choices, story);
+          }
+
+        } else {
+          story.currentSceneIndex++;
+          changeScene(story);
+        }
       });
-      container.appendChild(optionsElement);
+
+      this.currentChoicesIndex++;
+
+      containerToPrintChoices.appendChild(nodeCloned);
     }
+
   }
 }
 
-// Obtener la lista de historias disponibles en la carpeta "stories" en json
+function printChoices(choices, story) {
+
+  let optionsContainer = document.querySelector(".options");
+  const optionsElement = document.createElement("div");
+  optionsElement.className = "options";
+
+  choices.forEach(choice => {
+    const optionButton = document.createElement("button");
+    optionButton.textContent = choice.text;
+    optionButton.className = "btn2";
+    optionButton.addEventListener("click", () => {
+      
+      if (choice.choices != null) {
+        
+        if (choice.choices.length > 0) {
+          
+          story.displayChoices(optionsElement, choice.choices, story);
+        } else {
+          story.currentSceneIndex++;
+          changeScene(story);
+        }
+      } else {
+        story.currentSceneIndex++;
+        changeScene(story);
+      }
+    });
+    
+    optionsElement.appendChild(optionButton);
+  });
+
+  return optionsElement;
+}
+
+function clearOptions(containerToRemoveContent) {
+  
+  while (containerToRemoveContent.firstChild) {
+    containerToRemoveContent.removeChild(containerToRemoveContent.firstChild);
+  }
+
+  return containerToRemoveContent;
+}
+
+function removeContainer(containerToDelete) {
+  
+  containerToDelete.remove();
+}
+
+function changeScene(story) {
+  
+  let optionsContainer = document.querySelector(".options");
+  let sceneContainer = document.querySelector(".scene");
+  removeContainer(optionsContainer);
+  clearOptions(sceneContainer);
+
+  story.displayScene();
+}
+
 fetch("stories/json/stories.json")
   .then(response => response.json())
   .then(data => {
@@ -61,50 +165,37 @@ fetch("stories/json/stories.json")
     console.error("Error al cargar la lista de historias (json):", error);
   });
 
-// Lo mismo en yaml
-/* fetch("stories/yaml/stories.yaml")
-  .then(response => response.text())
-  .then(yamlData => {
-    const jsonData = yaml.load(yamlData);
-    const storyList = jsonData.stories;
+async function fetchStoryData() {
+  await fetch("stories/json/stories.json")
+  .then(response => response.json())
+  .then(data => {
+    const storyList = data.stories;
     showStoryOptions(storyList);
   })
   .catch((error) => {
-    console.error("Error al cargar la lista de historias (yaml):", error);
-  }); */
+    console.error("Error al cargar la lista de historias (json):", error);
+  });
+}
 
-// Mostrar las opciones de historias al usuario
 function showStoryOptions(storyList) {
   const storyContainer = document.getElementById("story-container");
 
-  // Crear el elemento de lista para las historias
   const storyListElement = document.createElement("ul");
 
-  // Iterar sobre las historias y agregar opciones a la lista
   storyList.forEach((story, index) => {
     const listItem = document.createElement("button");
     listItem.textContent = `${story.title}`;
     listItem.className = "btn2";
-    listItem.addEventListener("onclick", () => {
-      loadStory(story.filename);
+    listItem.addEventListener("click", () => {
+      loadStory(story.file);
     });
-    /* Por Implementar
-    // Create the snap for the ribbon
-    if (story.label != null) {
-      const snap = document.createElement("snap");
-      snap.textContent = story.label;
-      snap.classList.add("ribbon","material-symbols-outlined");
-      listItem.appendChild(snap);
-    }
-    */
+
     storyListElement.appendChild(listItem);
   });
 
-  // Agregar la lista de historias al contenedor
   storyContainer.appendChild(storyListElement);
 }
 
-// Cargar la historia seleccionada
 function loadStory(filename) {
   fetch(`stories/json/${filename}.json`)
     .then(response => response.json())
@@ -116,17 +207,3 @@ function loadStory(filename) {
       console.error("Error al cargar la historia (json):", error);
     });
 }
-
-// Lo mismo en yaml
-/* function loadStory(filename) {
-  fetch(`stories/yaml/${filename}.yaml`)
-    .then(response => response.text())
-    .then(yamlData => {
-      const jsonData = load(yamlData);
-      const story = new Story(jsonData);
-      story.run();
-    })
-    .catch((error) => {
-      console.error("Error al cargar la historia (yaml):", error);
-    });
-} */
